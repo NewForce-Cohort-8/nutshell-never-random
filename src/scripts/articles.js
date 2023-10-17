30c8ce6d71869f1f57795f9d7d22f359fd68a26e
Colin Matteson <seefm@proton.me>
2023-10-17 19:58:34 -0400

    collective database buildout and initial buildout of news article rendering from database

---
+++ src/scripts/articles.js
@@ -0,0 +1,22 @@
+//Authored by Colin Freeman Matteson the absolute goofiest coder of all time. This module takes the articles from the database and renders them to HTML once a user has logged in. Or it should, at least. Who knows if it actually works.
+
+import {getArticles} from "./dataAccess.js";
+
+const contentTarget = document.querySelector(".dashboard")
+
+const render = () => {
+    const articles = getArticles();
+    for (const article of articles) {
+        contentTarget.innerHTML += `<section class="article" id="article-${article.id}">
+        <h2>${article.title}</h2>
+        <button><a href="${article.url}" target="_blank">Read Now</a></button>
+        <p>Synopsis: ${article.synopsis}</p>
+        </section>
+        <hr>`
+    }
+}
+
+export const articleHTML = () => {
+    render()
+}
+
